69a5b745ab856b07b526112f1a22cf862b6b3ffa
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2019-01-29 17:38:00 +0000

    Include makew in this repo

--- Labs/starfield/configure.sh
+++ Labs/starfield/configure.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
 
-git --version
-echo
-make --version
-echo
-gcc --version
-echo
-clang --version
-echo
-
-git clone https://github.com/qqii/cmake-wrapper
-
-mv cmake-wrapper/cmakew.bat .
-mv cmake-wrapper/cmakew.sh .
-
-rm -rf cmake-wrapper
+git clone https://github.com/Microsoft/vcpkg
---
+++ Labs/starfield/version.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+git --version
+echo
+make --version
+echo
+gcc --version
+echo
+clang --version
+echo
